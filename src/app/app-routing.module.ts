bcbfcc03428024edd0430a4db9e7dc8339c755cf
Charith <charithboddu1996@gmail.com>
2023-02-12 23:47:26 -0500

    2/12 commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CameraComponent } from './components/layout/camera/camera.component';
 import { FormComponent } from './components/layout/form/form.component';
 import { HomeComponent } from './components/layout/home/home.component';
 import { VerifydetailsComponent } from './components/layout/verifydetails/verifydetails.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'Home', component: HomeComponent, data: {title: 'Home'} },
   { path: 'Forms', component: FormComponent, data: {title: 'Forms'} },
   { path: 'Verify-Details', component: VerifydetailsComponent, data: {title: 'Verify details'} },
+  { path: 'camera', component: CameraComponent, data: {title: 'Camera'} },
 
 ];
 
